db062dcf2bf657851161d583f5c7084c3fa7ad51
febrasko <105153916+febrasko@users.noreply.github.com>
2022-10-15 22:44:52 -0300

    corrigir indentação

--- conteudo.php
+++ conteudo.php
@@ -20,14 +20,14 @@
             echo "<p>Preencha todos os campos!</p>";
         } else {
     ?>
-        <div class="areatitulo">
-        <img src="img/chainsawbanner.jpg" width="100%">
-        <h1 class="titulo"> <?= $titulo ?> </h1>
-        </div>
-        <section>
-            <p> <?= $conteudo ?> </p>
-            <p class="direita"> <?= $autor ?> </p>
-        </section>
+    <div class="areatitulo">
+    <img src="img/chainsawbanner.jpg" width="100%">
+    <h1 class="titulo"> <?= $titulo ?> </h1>
+    </div>
+    <section>
+        <p> <?= $conteudo ?> </p>
+        <p class="direita"> <?= $autor ?> </p>
+    </section>
     <?php
         }
     }
